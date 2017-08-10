70e6164223659c65c0b68253a1bb6fa34385d52e
betterclever <paliwal.pranjal83@gmail.com>
2017-08-11 05:06:57 +0530

    Added Speech output for RSS and Table Output.

--- main/states/busy_state.py
+++ main/states/busy_state.py
@@ -8,6 +8,7 @@ class BusyState(State):
     """Busy state inherits from base class State. In this state, SUSI API is called to perform query and the response
     is then spoken with the selected Text to Speech Service.
     """
+
     def on_enter(self, payload=None):
         """This method is executed on entry to Busy State. SUSI API is called via SUSI Python library to fetch the
         result. We then call TTS to speak the reply. If successful, we transition to Idle State else to the Error State.
@@ -23,9 +24,31 @@ class BusyState(State):
             else:
                 self.__speak("I don't have an answer to this")
                 self.transition(self.allowedStateTransitions.get('idle'))
+
+            if 'table' in reply.keys():
+                table = reply['table']
+                for h in table.head:
+                    print('%s\t' % h, end='')
+                    self.__speak(h)
+                print()
+                for datum in table.data[0:4]:
+                    for value in datum:
+                        print('%s\t' % value, end='')
+                        self.__speak(value)
+                    print()
+
+            if 'rss' in reply.keys():
+                rss = reply['rss']
+                entities = rss['entities']
+                count = rss['count']
+                for entity in entities[0:count]:
+                    print(entity.title)
+                    self.__speak(entity.title)
+
         except ConnectionError:
             self.transition(self.allowedStateTransitions.get('error'), payload='ConnectionError')
-        except:
+        except Exception as e:
+            print(e)
             self.transition(self.allowedStateTransitions.get('error'))
 
     def on_exit(self):
