f33f69129eed3ac4baade052337e6e7e5af79703
Sanskar <sansyrox@gmail.com>
2018-05-31 04:33:08 +0530

    Added way for factory reset

---
+++ factory_reset/factory_reset.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# To be executed using a physical button
+
+cd /home/pi/SUSI.AI/susi_linux
+pwd
+mv susi_linux/ susi_temp
+git clone https://github.com/fossasia/susi_linux #while testing change to personal repo
+pwd
+ls
+cd susi_linux
+
+rm -rf ../susi_temp
+
+./install.sh
