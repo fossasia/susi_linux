ad09ed03789067a4a526090a809279ff5751f0ed
Sanskar Jethi <ssanskarjethi@Gauravs-MacBook-Pro.local>
2018-07-07 03:18:19 +0530

    Update

---
+++ main/hotword_engine/stop_detector.py
@@ -0,0 +1,33 @@
+import snowboydecoder
+import sys
+import signal
+
+interrupted = False
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
+if len(sys.argv) == 1:
+    print("Error: need to specify model name")
+    print("Usage: python demo.py your.model")
+    sys.exit(-1)
+
+model = sys.argv[1]
+
+signal.signal(signal.SIGINT, signal_handler)
+
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+print('Listening... Press Ctrl+C to exit')
+
+def stop():
+    print("stop")
+
+detector.start(detected_callback=stop,
+               interrupt_check=interrupt_callback)
+
+detector.terminate()
