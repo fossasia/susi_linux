73e30d1dda4c5a5765ac45a5ff1052be7119144c
Sanskar <sansyrox@gmail.com>
2018-07-14 02:04:02 +0530

    Removed Hardcoded paths

--- media_daemon/autostart.sh
+++ media_daemon/autostart.sh
@@ -2,10 +2,13 @@
 
 clear
 
-cd $HOME/SUSI.AI/susi_linux/
+SCRIPT_PATH=$(realpath $0)
+DIR_PATH=$(dirname $SCRIPT_PATH)
+
+cd $DIR_PATH/../susi_server/
 if [ -d "susi_server" ]
 then
-    cd $HOME/SUSI.AI/susi_linux/media_daemon/
+    cd $DIR_PATH
     python3 auto_skills.py
 else
     echo "Please download Skill Data"
