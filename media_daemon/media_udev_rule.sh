35cc9bff81e6a18cb44f43f0fbfb04e747b1b4e1
woshikie <woshikie@users.noreply.github.com>
2018-07-17 04:32:48 +0800

    Removed echoing comments into rule file

--- media_daemon/media_udev_rule.sh
+++ media_daemon/media_udev_rule.sh
@@ -6,9 +6,8 @@ SCRIPT_PATH=$(realpath $0)
 DIR_PATH=$(dirname $SCRIPT_PATH)
 RULE_NAME="99-susi-usb-drive.rules"
 
+# Ref: https://unix.stackexchange.com/questions/229987/udev-rule-to-match-any-usb-storage-device
 echo -e "\
-# Ref: https://unix.stackexchange.com/questions/229987/udev-rule-to-match-any-usb-storage-device\n\
-\n
 ACTION==\"add\", KERNEL==\"sd?\", SUBSYSTEM==\"block\", ENV{ID_BUS}==\"usb\", RUN=\"$DIR_PATH/autostart.sh\"\n\
 ACTION==\"remove\", KERNEL==\"sd?\", SUBSYSTEM==\"block\", ENV{ID_BUS}==\"usb\", RUN=\"$DIR_PATH/autostop.sh\"" > $DIR_PATH/$RULE_NAME
 
