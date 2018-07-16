2e6c9d1fda5c3e3935e043924a249d8a04568fb5
woshikie <woshikie@gmail.com>
2018-07-16 12:07:27 +0800

    fix codacy

--- main/states/led.py
+++ main/states/led.py
@@ -14,7 +14,9 @@ class LED_COLOR:
 
     def __init__(self, num_led, global_brightness=MAX_BRIGHTNESS,
                  order='rgb', bus=0, device=1, max_speed_hz=8000000):
-        self.driverInstalled = subprocess.check_output(["aplay", "-l"]).decode(sys.stdout.encoding).find("seeed") != -1
+        output = subprocess.check_output(["aplay", "-l"])
+        output = output.decode(sys.stdout.encoding)
+        self.driverInstalled = output.find("seeed") != -1
         if (not self.driverInstalled):
             return
         self.num_led = num_led  # The number of LEDs in the Strip
