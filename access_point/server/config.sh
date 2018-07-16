4e1992e52b092e70f4329d98480edc1ef57dc8e1
Sanskar <sansyrox@gmail.com>
2018-07-17 04:48:05 +0530

    Added fixes for the python server

--- access_point/server/config.sh
+++ access_point/server/config.sh
@@ -10,3 +10,10 @@ WAKE="$4"
 
 cd $DIR_PATH/../../
 sudo python3 config_generator.py $STT $TTS $HOTWORD $WAKE
+
+sudo systemctl daemon-reload
+sudo systemctl disable ss-python-flask.service
+sudo systemctl enable ss-susi-linux.service
+
+cd $DIR_PATH
+sudo ./rwap.sh
