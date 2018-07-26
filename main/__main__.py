8892a686edffd70f24dc2a261fbe22c3713ae0b9
Sanskar Jethi <sansyrox@gmail.com>
2018-07-26 06:38:33 +0530

    Fixes Travis and Codacy Bugs

--- main/__main__.py
+++ main/__main__.py
@@ -1,14 +1,11 @@
 from main import SusiStateMachine
 import json_config
-import requests
-import json
 import susi_python as susi
 
 config = json_config.connect('config.json')
 
 def add_device():
-    susi.sign_in(config['login_credentials']['email'],config['login_credentials']['password']
-    , speaker_name = config['speaker_name'], room_name = config['room_name'])
+    susi.sign_in(config['login_credentials']['email'], config['login_credentials']['password'], speaker_name = config['speaker_name'], room_name = config['room_name'])
 
 
 if __name__ == '__main__':
