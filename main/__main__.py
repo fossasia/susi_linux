e054b12cf7c5b9efca618870f7894761ff176a3c
Sanskar Jethi <sansyrox@gmail.com>
2018-07-26 17:10:58 +0530

    Fixes travis error

--- main/__main__.py
+++ main/__main__.py
@@ -4,8 +4,9 @@ import susi_python as susi
 
 config = json_config.connect('config.json')
 
+
 def add_device():
-    susi.sign_in(config['login_credentials']['email'], config['login_credentials']['password'], speaker_name = config['speaker_name'], room_name = config['room_name'])
+    susi.sign_in(config['login_credentials']['email'], config['login_credentials']['password'], speaker_name=config['speaker_name'], room_name=config['room_name'])
 
 
 if __name__ == '__main__':
