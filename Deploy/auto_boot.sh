7eaea2f63a1164c9ab6709787c7dea7238e0c586
Sanskar Jethi <sansyrox@gmail.com>
2018-07-27 13:33:32 +0400

    Proper workflow configured in accordance to android app (#275)
    * Added fixes for the python server
    
    * Removed RemainAfterExit

--- Deploy/auto_boot.sh
+++ Deploy/auto_boot.sh
@@ -3,7 +3,12 @@
 SCRIPT_PATH=$(realpath $0)
 DIR_PATH=$(dirname $SCRIPT_PATH)
 
-cp $DIR_PATH/ss-update-daemon.service /lib/systemd/system/ss-update-daemon.service
+cp $DIR_PATH/Systemd/ss-update-daemon.service /lib/systemd/system/ss-update-daemon.service
+cp $DIR_PATH/Systemd/ss-susi-server.service /lib/systemd/system/ss-susi-server.service
+cp $DIR_PATH/Systemd/ss-python-flask.service /lib/systemd/system/ss-python-flask.service
+cp $DIR_PATH/Systemd/ss-susi-linux.service /lib/systemd/system/ss-susi-linux.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable ss-update-daemon.service
+sudo systemctl enable ss-susi-server.service
+sudo systemctl enable ss-python-flask.service
