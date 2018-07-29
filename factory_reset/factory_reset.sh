77b10a1a8c5d392a24a4dc8cf42e3596ca5aeb83
Sanskar Jethi <sansyrox@gmail.com>
2018-07-30 02:21:27 +0530

    Final Update

--- factory_reset/factory_reset.sh
+++ factory_reset/factory_reset.sh
@@ -7,8 +7,8 @@ DIR_PATH=$(dirname $SCRIPT_PATH)
 cd $DIR_PATH/
 pwd
 
-sudo tar xvf $DIR_PATH/reset_folder.tar.gz  # extracting the backup folder
-mv $DIR_PATH/../susi_linux $DIR_PATH/../../susi_temp  # moving the backup file as a temp file
+sudo tar -c $DIR_PATH/ xvf $DIR_PATH/reset_folder.tar.gz  # extracting the backup folder
+mv $DIR_PATH/susi_linux $DIR_PATH/../../susi_temp  # moving the backup file as a temp file
 mv $DIR_PATH/../../susi_linux $DIR_PATH/../../susi_temp2  # making the original file to a temp file to delete it
 mv $DIR_PATH/../susi_temp $DIR_PATH/../../susi_linux  # making the backupfile as a new one
 
