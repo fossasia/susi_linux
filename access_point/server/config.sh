5f9dba3619eb2e936b0404f42f986e39b47bbe33
Sanskar <sansyrox@gmail.com>
2018-07-31 01:59:05 +0530

    Configured SUSI to run on startup with sound animations

--- access_point/server/config.sh
+++ access_point/server/config.sh
@@ -14,6 +14,7 @@ sudo python3 config_generator.py $STT $TTS $HOTWORD $WAKE
 sudo systemctl daemon-reload
 sudo systemctl disable ss-python-flask.service
 sudo systemctl enable ss-susi-linux.service
+sudo systemctl enable ss-factory-daemon.service
 
 sleep 5
 cd $DIR_PATH/../
