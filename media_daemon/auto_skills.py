0f7d691045eea01fca1184f837a9a15aff3a4308
Sanskar <sansyrox@gmail.com>
2018-08-05 22:05:59 +0530

    Fixed Updates according to commits

--- media_daemon/auto_skills.py
+++ media_daemon/auto_skills.py
@@ -9,20 +9,20 @@ media_daemon_folder = os.path.dirname(os.path.abspath(__file__))
 base_folder = os.path.dirname(media_daemon_folder)
 server_skill_folder = os.path.join(base_folder, 'susi_server/susi_server/data/generic_skills/media_discovery')
 server_settings_folder = os.path.join(base_folder, 'susi_server/susi_server/data/settings')
-server_restart_folder = os.path.join(base_folder, 'susi_server/susi_server/bin/restart.sh')
+server_restart_script = os.path.join(base_folder, 'susi_server/susi_server/bin/restart.sh')
 
 def make_skill(): # pylint-enable
     devices = list_media_devices()
     path = get_media_path(devices[0])
     subprocess.call(['udisks','mount',path])  #nosec #pylint-disable type: ignore
     name_of_usb = get_mount_points()
-    USB = name_of_usb[1]
-    os.chdir(str(USB))
+    usb = name_of_usb[1]
+    os.chdir(str(usb))
     mp3_files = glob("*.mp3")
     f = open( media_daemon_folder +'/custom_skill.txt','w')
     music_path = list()
     for mp in mp3_files:
-        music_path.append("{}".format(USB) + "/{}".format(mp))
+        music_path.append("{}".format(usb) + "/{}".format(mp))
 
     song_list = " ".join(music_path)
     skills = ['play audio','!console:Playing audio from your usb device','{"actions":[','{"type":"audio_play", "identifier_type":"url", "identifier":"file://'+str(song_list) +'"}',']}','eol']
@@ -32,7 +32,7 @@ def make_skill(): # pylint-enable
     shutil.move(os.path.join(media_daemon_folder, 'custom_skill.txt'), server_skill_folder)
     with open(os.path.join(server_settings_folder, 'customized_config.properties'), 'a') as f2:
         f2.write('local.mode = true')
-    subprocess.call(['sudo','bash',server_restart_folder])
+    subprocess.call(['sudo', 'bash', server_restart_script])  #nosec #pylint-disable type: ignore
 
 def get_usb_devices():
     sdb_devices = map(os.path.realpath, glob('/sys/block/sd*'))
