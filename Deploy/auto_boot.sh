df1f6441c82548cbadd7fbac7f03659c1c3b0990
Sanskar Jethi <sansyrox@gmail.com>
2018-08-14 23:57:26 +0530

    Update auto_boot.sh

--- Deploy/auto_boot.sh
+++ Deploy/auto_boot.sh
@@ -7,7 +7,7 @@ cp $DIR_PATH/Systemd/ss-update-daemon.service /lib/systemd/system/ss-update-daem
 cp $DIR_PATH/Systemd/ss-susi-server.service /lib/systemd/system/ss-susi-server.service
 cp $DIR_PATH/Systemd/ss-python-flask.service /lib/systemd/system/ss-python-flask.service
 cp $DIR_PATH/Systemd/ss-susi-linux.service /lib/systemd/system/ss-susi-linux.service
-cp $DIR_PATH/Systemd/ss-susi-startup.service /lib/systemd/system/ss-susi-startup.service
+cp $DIR_PATH/Systemd/ss-startup-audio.service /lib/systemd/system/ss-startup-audio.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable ss-update-daemon.service
