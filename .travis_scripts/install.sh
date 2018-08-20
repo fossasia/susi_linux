85570b5bcff476fc8ae82598ca4ae73ca556c3e3
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2018-08-20 22:09:08 +0700

    Update TravisCI script to adapt to new install script
    Remove some steps that we no longer need.

--- .travis_scripts/install.sh
+++ .travis_scripts/install.sh
@@ -1,108 +1,53 @@
 #!/bin/bash
 set -e
+SCRIPT_PATH=$(realpath $0)
+DIR_PATH=$(dirname $SCRIPT_PATH)
 
-# Implementation from https://stackoverflow.com/questions/4023830/how-compare-two-strings-in-dot-separated-version-format-in-bash
-vercomp()
-{
-    if [[ $1 == $2 ]]
-    then
-        echo 0;
-        return 0;
-    fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            echo 1;
-            return 0;
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            echo 2;
-            return 0;
-        fi
-    done
-    echo 0;
+
+add_fossasia_repo() {
+    echo "Set pip repo for root"
+    if ! sudo test -d /root/.pip; then sudo mkdir /root/.pip; fi
+    echo -e "[global]\nextra-index-url=https://repo.fury.io/fossasia/" | sudo tee /root/.pip/pip.conf
+    echo "Set pip repo for current user"
+    if [ ! -d ~/.config/pip ]; then mkdir -p ~/.config/pip ; fi
+    echo -e "[global]\nextra-index-url=https://repo.fury.io/fossasia/" > ~/.config/pip/pip.conf
 }
 
-function install_swig_from_sources()
-{
-    wget https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz
-    tar xf swig-3.0.12.tar.gz
-    cd swig-3.0.12
-    ./configure
-    make
-    sudo make install
-    rm -rf swig-3.0.12*
-    cd ..
+add_debian_repo() {
+    echo "Add ReSpeaker Debian repo"
+    # Respeaker driver https://github.com/respeaker/deb
+    wget -qO- http://respeaker.io/deb/public.key | sudo apt-key add -
+    echo "deb http://respeaker.io/deb/ stretch main" | sudo tee /etc/apt/sources.list.d/respeaker.list
+    sudo apt update
 }
 
-function install_flite_from_source()
+install_debian_dependencies()
 {
-    wget http://www.festvox.org/flite/packed/flite-2.0/flite-2.0.0-release.tar.bz2
-    tar xf flite-2.0.0-release.tar.bz2
-    cd flite-2.0.0-release
-    ./configure
-    make
-    sudo make install
-    rm -rf flite-2.0.0-release*
+    sudo -E apt install -y build-essential python3-pip sox libsox-fmt-all flac pulseaudio libpulse-dev \
+    python3-cairo python3-flask mpv flite ca-certificates-java
+    # We specify ca-certificates-java instead of openjdk-(8/9)-jre-headless, so that it will pull the
+    # appropriate version of JRE-headless, which can be 8 or 9, depending on ARM6 or ARM7 platform.
+}
+
+function install_seed_voicecard_driver()
+{
+    echo "installing Respeaker Mic Array drivers from source"
+    git clone https://github.com/respeaker/seeed-voicecard.git
+    cd seeed-voicecard
+    sudo ./install.sh
     cd ..
+    tar czf ~/seeed-voicecard.tar.gz seeed-voicecard
+    rm -rf seeed-voicecard
 }
 
 function install_dependencies()
 {
-    if /usr/bin/dpkg --search /usr/bin/dpkg
-    then
-        if ! [ -x "$(command -v swig)" ]
-        then
-            install_swig_from_sources
-        else
-            installed_version=$(swig -version | perl -nae 'print "$F[2]\n" if /SWIG Version/i;')
-            minimum_version="3.0.10"
-            result=$(vercomp ${installed_version} ${minimum_version})
-            if [ ${result} -eq 2 ]
-            then
-                echo "Installing SWIG 3.0.12 from sources"
-                install_swig_from_sources
-            else
-                echo "SWIG version is up to date"
-            fi
-        fi
-        sudo -E apt install libatlas-dev libatlas-base-dev
-    else
-        return 1;
-    fi
+    install_seed_voicecard_driver
 }
 
-function install_snowboy()
-{
-    if install_dependencies
-    then
-        root_dir=$(pwd)
-        sudo pip3 install git+https://github.com/Kitt-AI/snowboy.git
-        if [ $? -ne 0 ]; then
-            echo "FAILED: Unable to make Snowboy Detect file. Please follow manual instructions at https://github.com/kitt-AI/snowboy"
-            echo "You may also use PocketSphinx Detector if you are unable to install snowboy on your machine"
-        else
-            echo "Snowboy Detect successfully installed"
-        fi
-        cd "$root_dir"
-    else
-        echo "FAILED: Installation of Snowboy on your system is not supported presently. Please follow manual instructions at https://github.com/kitt-AI/snowboy"
-    fi
-}
+####  Main  ####
+add_fossasia_repo
+add_debian_repo
 
 echo "Downloading dependency: Susi Python API Wrapper"
 if [ ! -d "susi_python" ]
@@ -115,22 +60,20 @@ then
     rm -rf susi_api_wrapper
 fi
 
-echo "Downloading Python Dependencies"
-pip3 install -r requirements.txt
-pip3 install -r requirements-hw.txt
+echo "Installing required Debian Packages"
+install_debian_dependencies
 
-if ! [ -x "$(command -v flite)" ]
-then
-    echo "Downloading and Installing Flite TTS"
-    install_flite_from_source
-fi
+echo "Installing Python Dependencies"
+sudo -H pip3 install -U pip wheel
+sudo -H pip3 install -r requirements.txt  # This is from susi_api_wrapper
+sudo -H pip3 install -r requirements-hw.txt
+sudo -H pip3 install -r requirements-special.txt
 
 echo "Downloading Speech Data for flite TTS"
 
 if [ ! -f "extras/cmu_us_slt.flitevox" ]
 then
-    wget "http://www.festvox.org/flite/packed/flite-2.0/voices/cmu_us_slt.flitevox"
-    mv cmu_us_slt.flitevox extras/
+    wget "http://www.festvox.org/flite/packed/flite-2.0/voices/cmu_us_slt.flitevox" -P extras
 fi
 
 echo "Setting up Snowboy"
@@ -141,7 +84,3 @@ echo
 echo
 
 install_snowboy
-
-echo "Setup Complete"
-
-echo "Run 'python3 -m main' to start"
