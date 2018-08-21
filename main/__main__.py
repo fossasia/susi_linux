144b9d2a5c097df239afd36b1c48ab3ec0249ad6
Sanskar Jethi <sansyrox@gmail.com>
2018-08-22 02:59:12 +0530

    Travis Fixes

--- main/__main__.py
+++ main/__main__.py
@@ -10,9 +10,11 @@ curr_folder = os.path.dirname(os.path.abspath(__file__))
 def add_device():
     susi.sign_in(config['login_credentials']['email'], config['login_credentials']['password'], room_name=config['room_name'])
 
+
 def startup_sound():
-    audio_file = os.path.join(curr_folder,'wav/ting-ting_susi_has_started.wav')
-    subprocess.Popen(['play',audio_file])  # nosec #pylint-disable type: ignore
+    audio_file = os.path.join(curr_folder, 'wav/ting-ting_susi_has_started.wav')
+    subprocess.Popen(['play', audio_file])  # nosec #pylint-disable type: ignore
+
 
 if __name__ == '__main__':
     add_device()
