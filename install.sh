7badf5808946d7cd1bb71dfe0dc6bd6eb2aa706b
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2018-08-22 12:15:21 +0700

    Install udisks for Media Discovery feature.

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ add_debian_repo() {
 install_debian_dependencies()
 {
     sudo -E apt install -y python3-pip sox libsox-fmt-all flac \
-    python3-cairo python3-flask mpv flite ca-certificates-java pixz
+    python3-cairo python3-flask mpv flite ca-certificates-java pixz udisks2
     # We specify ca-certificates-java instead of openjdk-(8/9)-jre-headless, so that it will pull the
     # appropriate version of JRE-headless, which can be 8 or 9, depending on ARM6 or ARM7 platform.
 }
