27259a6e19b26c44fe9bcdf1ed57131722a1466b
Sanskar <sansyrox@gmail.com>
2018-10-03 18:56:32 +0530

    Added some scripts to emulate the image via qemu

---
+++ test/travis_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get install qemu
+
+git clone https://github.com/dhruvvyas90/qemu-rpi-kernel.git #jessie
+
+qemu-img convert -f raw -O qcow2 2017-08-16-raspbian-stretch-lite.zip raspbian-stretch-lite.qcow
+
+qemu-img resize raspbian-stretch-lite.qcow +6G
+
+sudo qemu-system-arm \
+-kernel ./kernel-qemu-4.4.34-jessie \
+-append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
+-hda raspbian-stretch-lite.qcow \
+-cpu arm1176 -m 256 \
+-M versatilepb \
+-no-reboot \
+-serial stdio \
+-net nic -net user \
+-net tap,ifname=vnet0,script=no,downscript=no
+
+cd ~/
+mkdir SUSI.AI
+git clone https://github.com/fossasia/susi_linux
+cd susi_linux/
+sudo bash ~/SUSI.AI/susi_linux/.travis_scripts/install.sh
