53555cca4be0124031a385d6fa6ec1fd01c15849
Sanskar Jethi <sansyrox@gmail.com>
2018-10-11 19:41:50 +0530

    Added white light control for RaspberryPi

---
+++ main/light_control/white_light_control.py
@@ -0,0 +1,9 @@
+""" This script has been created to control the random lights that are started
+on the ReSpeaker and also to add custom light animations in the future
+"""
+
+import sys
+sys.path.append("..")
+from states import lights
+
+lights.lights.off()
