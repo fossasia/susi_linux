284d010cee941176c0a45759fdd658b2c6d5f85d
Sanskar <sansyrox@gmail.com>
2018-10-20 18:05:28 +0530

    Update

--- test/travis_script.sh
+++ test/travis_script.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-sudo apt-get install qemu
+mkdir QEMU
+cd QEMU
 
-git clone https://github.com/dhruvvyas90/qemu-rpi-kernel.git #jessie
+# Downloading and installing QEMU from the source
+wget https://download.qemu.org/qemu-2.12.0-rc0.tar.xz
+tar xvJf qemu-2.12.0-rc0.tar.xz
+cd qemu-2.12.0-rc0
+./configure --target-list=arm-softmmu,aarch64-softmmu
+make
 
-qemu-img convert -f raw -O qcow2 2017-08-16-raspbian-stretch-lite.zip raspbian-stretch-lite.qcow
+# Downloading the latest Raspbian image
+wget https://downloads.raspberrypi.org/raspbian_lite_latest
+mv raspbian_lite_latest raspbian_lite_latest.zip
+unzip raspbian_lite_latest.zip
 
-qemu-img resize raspbian-stretch-lite.qcow +6G
+qemu-img convert -f raw -O qcow2 2018-10-09-raspbian-stretch-lite.img raspbian-lite.qcow2
 
-sudo qemu-system-arm \
--kernel ./kernel-qemu-4.4.34-jessie \
--append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
--hda raspbian-stretch-lite.qcow \
--cpu arm1176 -m 256 \
--M versatilepb \
--no-reboot \
--serial stdio \
--net nic -net user \
--net tap,ifname=vnet0,script=no,downscript=no
+qemu-img resize raspbian-lite.qcow2 +8G
 
-cd ~/
-mkdir SUSI.AI
-git clone https://github.com/fossasia/susi_linux
-cd susi_linux/
-sudo bash ~/SUSI.AI/susi_linux/.travis_scripts/install.sh
+# Downloading the kernel
+wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-4.4.34-jessi
+
+
+qemu-system-arm \
+        -kernel /kernel-qemu-4.4.34-jessie \
+        -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
+        -hda /raspbian-lite.qcow2 \
+        -cpu arm1176 \
+        -m 256 \
+        -machine versatilepb
