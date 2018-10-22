6e3498fb098e5c77dfc0f2c58d2233f63307aa54
Sanskar <sansyrox@gmail.com>
2018-10-23 01:35:50 +0530

    Update2

--- test/travis_script.sh
+++ test/travis_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 mkdir QEMU
 cd QEMU
 
@@ -31,30 +30,3 @@ qemu-system-arm \
         -cpu arm1176 \
         -m 256 \
         -machine versatilepb
-=======
-sudo apt-get update
-sudo apt-get install qemu-kvm qemu virt-manager virt-viewer libvirt-bin
-
-git clone https://github.com/dhruvvyas90/qemu-rpi-kernel.git
-
-qemu-img convert -f raw -O qcow2 2017-08-16-raspbian-stretch-lite.zip raspbian-stretch-lite.qcow
-
-qemu-img resize raspbian-stretch-lite.qcow +6G
-
-sudo qemu-system-arm \
--kernel ./kernel-qemu-4.9.59-stretch \
--append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
--hda raspbian-stretch-lite.qcow \
--cpu arm1176 -m 256 \
--M versatilepb \
--no-reboot \
--serial stdio \
--net nic -net user \
--net tap,ifname=vnet0,script=no,downscript=no
-
-cd ~/
-mkdir SUSI.AI
-git clone https://github.com/fossasia/susi_linux
-cd susi_linux/
-sudo bash ~/SUSI.AI/susi_linux/.travis_scripts/install.sh
->>>>>>> ccc7e0a5a2ac93a787987f446b49971e3a7d93f7
