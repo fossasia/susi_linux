88e6a1b528f30a31da7342582e89d8dc0419b29f
Sanskar <sansyrox@gmail.com>
2018-10-24 02:38:29 +0530

    Trying to make images via the rc.local file

---
+++ test/image_creation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd /home/pi/
+
+# if statement
+if [ -d "SUSI.AI"]; 
+then
+    rm -rf SUSI.AI
+fi
+
+if [! -d "SUSI.AI"]; 
+then
+    mkdir SUSI.AI
+fi
+
+cd SUSI.AI
+
+git clone https://github.com/fossasia/susi_linux
+
+cd susi_linux/travis/
+
+chmod +x travis_install.sh
+
+./travis_install.sh
