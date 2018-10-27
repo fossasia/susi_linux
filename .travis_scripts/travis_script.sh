7752d57e6ba245799f8bbca041914a2c308929c1
Sanskar <sansyrox@gmail.com>
2018-10-28 00:59:39 +0530

    Final Update

--- .travis_scripts/travis_script.sh
+++ .travis_scripts/travis_script.sh
@@ -10,23 +10,19 @@ cd qemu-2.12.0-rc0
 ./configure --target-list=arm-softmmu,aarch64-softmmu
 make
 
-# Downloading the latest Raspbian image
-wget https://downloads.raspberrypi.org/raspbian_lite_latest
-mv raspbian_lite_latest raspbian_lite_latest.zip
-unzip raspbian_lite_latest.zip
-
-qemu-img convert -f raw -O qcow2 2018-10-09-raspbian-stretch-lite.img raspbian-lite.qcow2
+# Downloading the latest Raspbian Qcow
+wget https://drive.google.com/uc?export=download&confirm=DBZl&id=1CJj3sefBr5cj3k-88WSAXbtITg1EU_hW
 
 qemu-img resize raspbian-lite.qcow2 +8G
 
 # Downloading the kernel
-wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-4.4.34-jessi
+wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu-4.4.34-jessie
 
 
 qemu-system-arm \
-        -kernel /kernel-qemu-4.4.34-jessie \
+        -kernel ./kernel-qemu-4.4.34-jessie \
         -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
-        -hda /raspbian-lite.qcow2 \
+        -hda ./raspbian-lite.qcow2 \
         -cpu arm1176 \
         -m 256 \
         -machine versatilepb
