1431bedf3018bd56486b0f788fd6e1c8da9bcb16
Michael Christen <Orbiter@users.noreply.github.com>
2019-03-05 21:40:54 +0100

    fix for factory reset after event-driven wake button change
    also: now wait for 10 seconds

--- factory_reset/factory_reset.py
+++ factory_reset/factory_reset.py
@@ -3,9 +3,7 @@ import time
 import logging
 import subprocess  # nosec #pylint-disable type: ignore
 
-import alsaaudio
 import RPi.GPIO as GPIO
-
 
 logger = logging.getLogger(__name__)
 current_folder = os.path.dirname(os.path.abspath(__file__))
@@ -13,29 +11,23 @@ factory_reset = os.path.join(current_folder, 'factory_reset.sh')
 
 try:
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17,GPIO.IN)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     i = 1
     while True:
         if GPIO.input(17) == 1:
-            time.sleep(0.1)
+            time.sleep(0.2)
             pass
         elif GPIO.input(17) == 0 :
             start = time.time()
             while GPIO.input(17) == 0 :
-                time.sleep(0.1)
+                time.sleep(0.2)
             end = time.time()
             total = end - start
-            if total >= 7 :
+            if total >= 10 :
+                print("FACTORY RESET")
                 subprocess.call(['bash','factory_reset.sh'])  # nosec #pylint-disable type: ignore
-            else :
-                mixer = alsaaudio.Mixer()
-                value = mixer.getvolume()[0]
-                if value != 0:
-                    mixer.setvolume(0)
-                else:
-                    mixer.setvolume(50)
             logger.info(total)
-            time.sleep(0.1)
+            time.sleep(0.2)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
