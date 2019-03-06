5ce196f90f4a8235011680a61ef28ea99f3f310c
Michael Christen <Orbiter@users.noreply.github.com>
2019-03-06 20:39:36 +0100

    bugfixes to factory reset

--- factory_reset/factory_reset.sh
+++ factory_reset/factory_reset.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # extract backup
-sudo tar -Ipixz -C $DIR_PATH/ -xf $DIR_PATH/reset_folder.tar.xz
+tar -Ipixz -C /home/pi/SUSI.AI/susi_linux/factory_reset/ -xf /home/pi/SUSI.AI/susi_linux/factory_reset/reset_folder.tar.xz
 
 # stop running processes
 sudo killall python3
@@ -12,11 +12,13 @@ mv /home/pi/SUSI.AI/susi_linux/factory_reset/susi_linux /home/pi/SUSI.AI/susi_te
 mv /home/pi/SUSI.AI/susi_linux /home/pi/SUSI.AI/susi_old
 mv /home/pi/SUSI.AI/susi_temp /home/pi/SUSI.AI/susi_linux
 
+# rescue the rescue dump
+mv /home/pi/SUSI.AI/susi_old/factory_reset/reset_folder.tar.xz /home/pi/SUSI.AI/susi_linux/factory_reset/
+
 # clean up
-rm -rf /home/pi/SUSI.AI/susi_old
+sudo rm -rf /home/pi/SUSI.AI/susi_old
 
 # prepare to run susi smart speaker as hot spot again
 sudo bash /home/pi/SUSI.AI/susi_linux/access_point/wap.sh
 
-# restart
-sudo shutdown -r now
+# restart: the wap script is doing the restart
