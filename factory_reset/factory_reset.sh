1098827dfef5824c84ed7467bd3f6dfaafa591de
Michael Christen <Orbiter@users.noreply.github.com>
2019-03-09 08:25:33 +0100

    removed sudo because the whole script is now called with sudo

--- factory_reset/factory_reset.sh
+++ factory_reset/factory_reset.sh
@@ -1,36 +1,33 @@
 #! /bin/bash
 
+# stop running processes
+killall python3
+killall java
+
 # extract backup
 tar -Ipixz -C /home/pi/SUSI.AI/susi_linux/factory_reset/ -xf /home/pi/SUSI.AI/susi_linux/factory_reset/reset_folder.tar.xz
-
-# stop running processes
-sudo killall python3
-sudo killall java
 
 # replace running version with backup
 mv /home/pi/SUSI.AI/susi_linux/factory_reset/susi_linux /home/pi/SUSI.AI/susi_temp
 mv /home/pi/SUSI.AI/susi_linux /home/pi/SUSI.AI/susi_old
 mv /home/pi/SUSI.AI/susi_temp /home/pi/SUSI.AI/susi_linux
 
-# rescue the rescue dump
+# rescue the rescue dump before cleaning up
 mv /home/pi/SUSI.AI/susi_old/factory_reset/reset_folder.tar.xz /home/pi/SUSI.AI/susi_linux/factory_reset/
 
 # clean up
-sudo rm -rf /home/pi/SUSI.AI/susi_old
+rm -rf /home/pi/SUSI.AI/susi_old
 
 # prepare to run susi smart speaker as hot spot again
 # here we undo the /home/pi/SUSI.AI/susi_linux/access_point/rwap.sh script
-cd /etc/hostapd/
-sudo cp hostapd.conf.bak hostapd.conf
-cd /etc/
-sudo cp dhcpcd.conf.bak dhcpcd.conf
-cd /etc/network/
-sudo cp interfaces.bak interfaces
+cp /etc/hostapd/hostapd.conf.bak /etc/hostapd/hostapd.conf
+cp /etc/dhcpcd.conf.bak /etc/dhcpcd.conf
+cp /etc/network/interfaces.bak /etc/network/interfaces
 
-sudo systemctl disable ss-startup-audio.service
-sudo systemctl disable ss-susi-linux.service
-sudo systemctl enable ss-python-flask.service
-sudo systemctl disable ss-susi-login.service
+systemctl disable ss-startup-audio.service
+systemctl disable ss-susi-linux.service
+systemctl enable ss-python-flask.service
+systemctl disable ss-susi-login.service
 
 # restart
-sudo reboot
+reboot
