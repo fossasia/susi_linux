1fa53e8eac7860dd278542181c123cf7db3bb887
Norbert Preining <norbert@preining.info>
2019-04-18 11:22:46 +0900

    remove installation specific stuff

--- media_daemon/autostop.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-SCRIPT_PATH=$(realpath $0)
-DIR_PATH=$(dirname $SCRIPT_PATH)
-
-cd $DIR_PATH/../susi_server/susi_server/data/generic_skills/media_discovery/
- 
-sudo rm custom_skill.txt 
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script is to run SUSI.AI automatically at bootup
-# The script is triggered through a Systemd rule `ss-factory-daemon.service`
-
-cd "`dirname $0`"
-
-nohup play wav/ting-ting_susi_is_alive_and_listening.wav &
-python3 -m main
