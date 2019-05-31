80de794fb6465655e34e7bed7b54cac552f794f4
Norbert Preining <norbert@preining.info>
2019-05-31 11:09:14 +0900

    allow direct playback by passing additional argument

--- main/player/__init__.py
+++ main/player/__init__.py
@@ -2,6 +2,7 @@
 
 import logging
 import requests
+from vlcplayer import vlcplayer
 
 baseurl = 'http://localhost:7070/'
 
@@ -11,28 +12,57 @@ def send_request(req):
     except Exception as e:
         logger.error(e)
 
-
 class Player():
-    def playytb(self, vid):
-        send_request('play?ytb=' + vid)
-    def play(self, mrl):
-        send_request('play?mrl=' + mrl)
-    def pause(self):
-        send_request('pause')
-    def resume(self):
-        send_request('resume')
-    def stop(self):
-        send_request('stop')
-    def beep(self, mrl):
-        send_request('beep?mrl=' + mrl)
-    def say(self, mrl):
-        send_request('say?mrl=' + mrl)
-    def volume(self, val):
-        send_request('volume?val=' + val)
-    def save_volume(self):
-        send_request('save_volume')
-    def restore_volume(self):
-        send_request('restore_volume')
+    def playytb(self, vid, use_server = True):
+        if use_server:
+            send_request('play?ytb=' + vid)
+        else:
+            vlcplayer.playytb(vid)
+    def play(self, mrl, use_server = True):
+        if use_server:
+            send_request('play?mrl=' + mrl)
+        else:
+            vlcplayer.play(mrl)
+    def pause(self, use_server = True):
+        if use_server:
+            send_request('pause')
+        else:
+            vlcplayer.pause()
+    def resume(self, use_server = True):
+        if use_server:
+            send_request('resume')
+        else:
+            vlcplayer.resume()
+    def stop(self, use_server = True):
+        if use_server:
+            send_request('stop')
+        else:
+            vlcplayer.stop()
+    def beep(self, mrl, use_server = True):
+        if use_server:
+            send_request('beep?mrl=' + mrl)
+        else:
+            vlcplayer.beep(mrl)
+    def say(self, mrl, use_server = True):
+        if use_server:
+            send_request('say?mrl=' + mrl)
+        else:
+            vlcplayer.say(mrl)
+    def volume(self, val, use_server = True):
+        if use_server:
+            send_request('volume?val=' + val)
+        else:
+            vlcplayer.volume(val)
+    def save_volume(self, use_server = True):
+        if use_server:
+            send_request('save_volume')
+        else:
+            vlcplayer.save_volume()
+    def restore_volume(self, use_server = True):
+        if use_server:
+            send_request('restore_volume')
+        else:
+            vlcplayer.restore_volume()
 
 player = Player()
 
