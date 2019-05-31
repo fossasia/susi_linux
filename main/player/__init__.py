b05b626b49d508a829d848d87954336974bd6919
Norbert Preining <norbert@preining.info>
2019-05-31 12:19:03 +0900

    allow autodetection of presence of soundserver, and on call select

--- main/player/__init__.py
+++ main/player/__init__.py
@@ -4,6 +4,8 @@ import logging
 import requests
 from vlcplayer import vlcplayer
 
+logger = logging.getLogger(__name__)
+default_mode = 'server'
 baseurl = 'http://localhost:7070/'
 
 def send_request(req):
@@ -13,56 +15,77 @@ def send_request(req):
         logger.error(e)
 
 class Player():
-    def playytb(self, vid, use_server = True):
-        if use_server:
+    def __init__(self, mode = None):
+        if mode is None:
+            mode = default_mode
+        if (not mode == 'server') and (not mode == 'direct'):
+            logger.error('unknown mode ', mode, ' trying default mode')
+            mode = default_mode
+        if mode == 'server':
+            # try to check whether server is available
+            try:
+                requests.get(baseurl + 'status')
+                self.mode = 'server'
+            except:
+                self.mode = 'direct'
+                logger.info('sound server not available, switching to direct play mode')
+        else:
+            self.mode = 'direct'
+        logger.info("Player is working in mode: ", self.mode)
+
+    def playytb(self, vid, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('play?ytb=' + vid)
         else:
+            print("==direct playing youtube video ", vid)
             vlcplayer.playytb(vid)
-    def play(self, mrl, use_server = True):
-        if use_server:
+    def play(self, mrl, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('play?mrl=' + mrl)
         else:
+            print("==direct playing mrl ", str(mrl))
             vlcplayer.play(mrl)
-    def pause(self, use_server = True):
-        if use_server:
+    def pause(self, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('pause')
         else:
             vlcplayer.pause()
-    def resume(self, use_server = True):
-        if use_server:
+    def resume(self, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('resume')
         else:
             vlcplayer.resume()
-    def stop(self, use_server = True):
-        if use_server:
+    def stop(self, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('stop')
         else:
             vlcplayer.stop()
-    def beep(self, mrl, use_server = True):
-        if use_server:
+    def beep(self, mrl, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('beep?mrl=' + mrl)
         else:
             vlcplayer.beep(mrl)
-    def say(self, mrl, use_server = True):
-        if use_server:
+    def say(self, mrl, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('say?mrl=' + mrl)
         else:
             vlcplayer.say(mrl)
-    def volume(self, val, use_server = True):
-        if use_server:
+    def volume(self, val, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('volume?val=' + str(val))
         else:
             vlcplayer.volume(val)
-    def save_volume(self, use_server = True):
-        if use_server:
+    def save_volume(self, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('save_volume')
         else:
             vlcplayer.save_volume()
-    def restore_volume(self, use_server = True):
-        if use_server:
+    def restore_volume(self, mode = None):
+        if (mode == 'server') or ((mode is None) and (self.mode == 'server')):
             send_request('restore_volume')
         else:
             vlcplayer.restore_volume()
+
 
 player = Player()
 
