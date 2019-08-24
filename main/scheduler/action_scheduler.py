7e7dfdf2c46992412dd8f200732fa40443005632
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2019-08-24 20:31:56 +0700

    Fix coding style.

--- main/scheduler/action_scheduler.py
+++ main/scheduler/action_scheduler.py
@@ -1,25 +1,26 @@
-from threading import Thread
-from rx.subject import Subject
+
 import time
 import sched
-import logging
-from pprint import pprint
+from threading import Thread
+
+from rx.subject import Subject
+
 
 class ActionScheduler(Thread):
 
     def __init__(self):
         super().__init__()
         self.subject = Subject()
-        self.events={}
-        self.counter=-1
+        self.events = {}
+        self.counter = -1
         self.scheduler = sched.scheduler(time.time, time.sleep)
 
     def on_detected(self, reply):
         self.subject.on_next(reply)
 
-    def add_event(self,time,reply):
-        self.events[self.counter+1] = self.scheduler.enter(time, 0, self.on_detected, argument=(reply,))
-        self.counter+=1
+    def add_event(self, time, reply):
+        self.events[self.counter + 1] = self.scheduler.enter(time, 0, self.on_detected, argument=(reply,))
+        self.counter += 1
 
     def run(self):
         while True:
--- main/states/susi_state_machine.py
+++ main/states/susi_state_machine.py
@@ -1,23 +1,23 @@
 """This module declares the SUSI State Machine Class and Component Class.
 The SUSI State Machine works on the concept of Finite State Machine.
 """
+import time
 import logging
 from threading import Thread
-import time
+from urllib.parse import urljoin
 
 import requests
 import json_config
-import susi_python as susi
+
 from speech_recognition import Recognizer, Microphone
 from requests.exceptions import ConnectionError
-from urllib.parse import urlencode
+
+import susi_python as susi
 from .busy_state import BusyState
 from .error_state import ErrorState
 from .idle_state import IdleState
 from .recognizing_state import RecognizingState
-from requests_futures.sessions import FuturesSession
 from ..scheduler import ActionScheduler
-
 
 
 logger = logging.getLogger(__name__)
@@ -94,13 +94,13 @@ class Components:
     def server_checker(self):
         response_one = None
         test_params = {
-        'q': 'Hello',
-        'timezoneOffset': int(time.timezone / 60)
+            'q': 'Hello',
+            'timezoneOffset': int(time.timezone / 60)
         }
         while response_one is None:
             try:
-                response_one = requests.get('{}/susi/chat.json?{}'
-                .format(self.server_url,urlencode(test_params)) ).result()
+                url = urljoin(self.server_url, '/susi/chat.json')
+                response_one = requests.get(url, test_params).result()
                 api_endpoint = self.server_url
                 susi.use_api_endpoint(api_endpoint)
             except AttributeError:
