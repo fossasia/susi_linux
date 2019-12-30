51ff59efef84a4f7898e6ef23c40b9ca91eeb51a
Norbert Preining <norbert@preining.info>
2019-12-30 22:51:28 +0900

    switch to the new susi state machine implementation

--- main/states/__init__.py
+++ main/states/__init__.py
@@ -1,4 +1,4 @@
 """This module defines all the states and their actions.
 """
-from .susi_state_machine import SusiStateMachine
-from .susi_state_machine import Components
+from .susi_state_machine_simple import SusiStateMachine
+from .susi_state_machine_simple import Components
